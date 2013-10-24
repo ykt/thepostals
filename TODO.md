eed32dc4c1a28f455dee7b936fc3b55d3f766aa7
Yasir Khalid <yasir.khalid@servicerocket.com>
2013-10-25 00:09:23 +0800

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,7 @@
+TODO
+=====
+
+- Use font Helvetica Neue Ultralight, install locally
+- Use Less.js
+- Parallax.js or Skroll.js
+- Resolution icon samain panjang ama lebarnya
---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Postal Projects</title>
+        
+
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <!-- Latest compiled and minified CSS sprite-glyphicons.png -->
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css"> -->
+
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.11/skrollr.min.js"></script>
+        <script type="text/javascript">
+			window.onload = function() {
+			    skrollr.init({
+			    	forceHeight: false
+				});
+    		}
+        </script>
+        <!-- It might be temporary this glyphicons sprite-->
+        <link href="css/bootstrap.icon-large.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <link href="../css/postal.css" rel="stylesheet">
+    </head>
+    <style> 
+
+
+</style>
+
+    <script type="text/javascript" >
+    </script>
+    <body>
+			<section class="row">
+	            <div class="col-md-offset-2 col-md-4 col-xs-5">
+	                <img src="../img/banner/pp_flag.png" class="img-responsive  vertical-line">
+	            </div>
+	            <div class="col-md-1 icon-banner">
+	                <img src="../img/banner/envelope_icon.png" class="hidden-xs">
+	            </div>
+	            <div class="col-md-1 icon-banner">
+	                <img src="../img/banner/stamps_icon.png" class="hidden-xs"> 
+	            </div>
+	            <div class="col-md-1 icon-banner">
+	                <img src="../img/banner/airmail_icon.png" class="hidden-xs">
+	            </div>
+	            <div class="col-md-offset-2 col-xs-12 col-md-8 main-border empty-2-em carousel slide" id="postal-carousel">
+		        	<div class="carousel-inner">
+			    	    <div class="item active">
+							<img src="http://www.clashmusic.com/sites/default/files/styles/article_feature/public/field/image/postal-service-2013.jpeg" style="width: 952px; height: 360px" class="img-respnsive">
+						</div>
+						<div class="item">
+							<img src="../img/banner/artwork_stamps.jpg" alt="Second slide" style="height: 360px; width: 100%; display: block;">
+							<div class="container">
+								<div class="carousel-caption">
+									<h1>XXXX is something</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a class="left carousel-control" href="#postal-carousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+					<a class="right carousel-control" href="#postal-carousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+<!-- 
+                	<img src="../img/banner/artwork_stamps.jpg" class="hidden-xs banner-cs text-center" style="width: 100%; height:370px"/>
+                	<img src="../img/headline_mobile.png" class="visible-xs banner-cs img-responsive"/> -->
+            	</div>    
+		    </section>
+		    <hr>
+	        <section class="row" data-0="height: 100%;" data-1000="height: 0px;">
+	        	<div class="col-md-offset-2 col-xs-12 col-md-8">
+                	<img src="../img/banner/stamps_page.png" class="img-responsive">
+            	</div>
+            </section>
+		    <hr>
+	        <section class="row">
+	        	<div class="col-md-offset-2 col-xs-12 col-md-8">
+                	<img src="../img/banner/airmail_page.png" class="img-responsive">
+            	</div>
+            </section>
+		    <hr>
+	        <section class="row">
+	        	<div class="col-md-offset-2 col-xs-12 col-md-8">
+                	<img src="../img/banner/envelope_page.png" class="img-responsive">
+            	</div>
+            </section>
+    </body>
+</html>
+<!-- repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 10px, #AC7C43 19px, #AC7C43 29px) -->
